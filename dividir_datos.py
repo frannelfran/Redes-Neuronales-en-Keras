d80b95124d7d8af1e5c26d12a0696adcb6534bd2
Franco Alla <frannelfran@gmail.com>
2026-02-09 17:41:11 +0000

    feat: Programa para dividir los datos en entrenamiento, validación y prueba

---
+++ dividir_datos.py
@@ -0,0 +1,32 @@
+import splitfolders
+import sys
+
+# 2. Configurar rutas
+# Carpeta donde están tus fotos ahora (por ejemplo, con subcarpetas 'gatos' y 'perros')
+input_folder = sys.argv[1]
+
+# Carpeta donde quieres el resultado ordenado
+output_folder = sys.argv[2]
+
+if (sys.argv[1] == None) or (sys.argv[2] == None):
+    print("Error: Debes proporcionar las rutas de entrada y salida.")
+    print("Uso: python dividir_datos.py <ruta_entrada> <ruta_salida>")
+    sys.exit(1)
+
+input_train = float(input("Porcentaje de entrenamiento: "))
+input_val = float(input("Porcentaje de validación: "))
+input_test = float(input("Porcentaje de prueba: "))
+
+if (input_train + input_val + input_test) != 100:
+    print("Error: La suma de los porcentajes debe ser 100.")
+    sys.exit(1)
+elif (input_train < 0) or (input_val < 0) or (input_test < 0):
+    print("Error: Los porcentajes no pueden ser negativos.")
+    sys.exit(1)
+
+# 3. Dividir (ratio=(train, validation, test))
+# Ejemplo: 80% entrenamiento, 10% validación, 10% prueba
+splitfolders.ratio(input_folder, output=output_folder, 
+                   seed=42, ratio=(input_train/100, input_val/100, input_test/100), group_prefix=True)
+
+print(f"¡Listo! Revisa la carpeta {output_folder} para ver los datos divididos.")
